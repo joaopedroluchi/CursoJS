70c669554c16791304a2ef5a4363c8af2b18531f
JoaoPedroLuchi <joaopedroluchis@gmail.com>
2022-10-17 15:12:14 -0300

    12

--- teste/qual sua idade/script.js
+++ teste/qual sua idade/script.js
@@ -2,17 +2,19 @@ function clicar(){
     var idade = window.document.getElementById('txtidade')
     var msgm  = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
+   
     var idade = txtidade.value
+   
     msgm.innerHTML = (`Você tem ${idade} anos.`)
 
-    if(idade >=1 && idade <= 12){
+    if(idade >=1 && idade <= 12 ){
         img.src = 'crianca.png'
         msgm.innerHTML = (`Você tem ${idade} anos, você é criança.`)
     }else{
         if(idade >=12 & idade <=25){
             img.src = 'jovem.png'
             msgm.innerHTML = (`Você tem ${idade} anos, você é jovem.`)
-        }else if(idade >=26 && idade <=50){
+        }else if(idade >=26 && idade <=49){
             img.src = 'adulto.png'
             msgm.innerHTML = (`Você tem ${idade} anos, você é adulto.`)
         } else if(idade >= 50){
